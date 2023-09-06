8265abd546a567255ead15e0a288805bd9088799
Ilya <elisov97@gmail.com>
2023-09-06 14:09:47 +0300

    task ok

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express')
 
-const loginRoute = require('./routes/login')
-const booksRoute = require('./routes/books')
+const loginApiRoute = require('./routes/loginApi')
+const booksApiRoute = require('./routes/books')
 const error404 = require('./middleware/err404')
 
 
 const app = express()
 app.use(express.json())
 
-app.use('/api/user/login', loginRoute)
-app.use('/api/books', booksRoute)
+app.use(express.urlencoded());
+app.set("view engine", "ejs");
+
+app.use('/api/user/login', loginApiRoute)
+app.use('/', booksApiRoute)
 
 app.use(error404)
 
